9f7b9efd994cbe89682cb0a1d120e2cfd549e1d2
GeorgieSmile <nguntasin16@gmail.com>
2025-08-15 19:23:22 +0700

    Handle capitalize when user enter qr as payment method

--- app_api/routers/sales.py
+++ app_api/routers/sales.py
@@ -17,6 +17,8 @@ def create_sale(sale: request_models.SaleCreate, db: db_dependency):
     """
 
     sale.payment_method = sale.payment_method.capitalize()
+    if sale.payment_method == 'Qr':
+        sale.payment_method = 'QR'
     if sale.payment_method not in ['Cash', 'Card', 'QR']:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="วิธีการชำระเงินไม่ถูกต้อง")
     # Create the sale record first, excluding items for now
