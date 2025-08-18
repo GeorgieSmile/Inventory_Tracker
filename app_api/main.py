2f68c06f19cec6d62ac311fd1c6e29ee7c0ce8a5
GeorgieSmile <nguntasin16@gmail.com>
2025-08-18 19:49:26 +0700

    Add CORS to main.py

--- app_api/main.py
+++ app_api/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from database import Base, engine
 from routers import categories, products, stocks, sales, inventories, report
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -10,6 +11,14 @@ app.include_router(stocks.router)
 app.include_router(sales.router)
 app.include_router(inventories.router)
 app.include_router(report.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
