442d16b8109c1854bcbb0f50f0cd770ed923a4fa
GeorgieSmile <nguntasin16@gmail.com>
2025-08-25 21:58:40 +0700

    Update config to make it work with docker

--- app_api/config.py
+++ app_api/config.py
@@ -1,9 +1,9 @@
-DATABASE_URL = "mysql+pymysql://root:password@localhost:3306/inventory_db"
+import os
 
-DB_USER = "root"
-DB_PASSWORD = "rootpass"  
-DB_HOST = "localhost"
-DB_NAME = "myapp"
+DB_USER = os.getenv("DB_USER", "root")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "rootpass")
+DB_HOST = os.getenv("DB_HOST", "localhost")          
+DB_NAME = os.getenv("DB_NAME", "myapp")
+DB_PORT = os.getenv("DB_PORT", "3306")
 
-# Build the URL
-DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:3306/{DB_NAME}"
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
